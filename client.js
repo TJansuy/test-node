0d44b78996bb51cf9fb0e07dbeb5e17938324893
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-04-28 16:44:46 -0700

    Adding some fun interaction between the server and clients.

--- client.js
+++ client.js
@@ -7,7 +7,7 @@ e.innerHTML = "Updated by client.js";
 // Find the buttons
 start = document.getElementById('btnStart');
 stop = document.getElementById('btnStop');
-
+send = document.getElementById('btnSend');
 
 // Begin button Functionality
 var keep = null;
@@ -16,8 +16,24 @@ var ticker = 0;
 //// Give the buttons text programatically
 start.innerHTML = "Start";
 stop.innerHTML = "Stop";
+send.innerHTML = "Send";
 
 //// Assign the onclick functionality after the script has had a chance to load.
 start.onclick = () => {if(keep === null) { keep = setInterval(() => {ticker = ticker+1; console.log(ticker); e.innerHTML = ticker;}, 100)};}; 
 stop.onclick = () => {clearInterval(keep); keep = null;};
+
+send.onclick = async () => {fetch("/secret")};
+
+async function getMessage() {
+	const response = await fetch("/messages");
+	const jsonData = await response.json();
+	console.log(jsonData);
+	if (jsonData['secretTrigger']) {
+		console.log("SURPRISE!")
+		const d = document.getElementById("secret");
+		d.innerHTML = '<iframe width="560" height="315" src="https://www.youtube.com/embed/4bMOTTJqGgM?start=64" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+	} 
+};
+
+setInterval(getMessage, 1000);
 
--- server.js
+++ server.js
@@ -6,16 +6,32 @@ const PORT = 8080;
 const HOST = '0.0.0.0';
 
 const app = express();
+
+// GET requests
 app.get('/', (req, res) => {
 	console.log(req);
 	res.send("hello world");
-}
-);
+});
 
+var secret = false;
+
+app.get('/messages', (req, res) => {
+	console.log("Messages checked " + secret + " : " + req.json());
+	res.json({secretTrigger: secret});
+	secret = false;	
+});
+
+// POST requests
 app.post('/user/:action/:target', (req, res) => {
 	console.log("user");
 	console.log(req.params);
 	res.send(req.params);
+});
+
+app.post('/secret', (req, res) => {
+	console.log("Secret Triggered");
+	secret = true;
+	res.send("Secret Triggered!");
 });
 
 app.use(express.static('/usr/src/app'));
