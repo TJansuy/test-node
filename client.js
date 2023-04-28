79b5d6d85f2b38a2a4dac6c65231126428dc888d
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-04-27 23:35:16 -0700

    Adding some basic button functionality to test client side javascript

--- client.js
+++ client.js
@@ -2,3 +2,16 @@ console.log("success");
 
 e = document.getElementById('foo');
 e.innerHTML = "Updated by client.js";
+
+start = document.getElementById('btnStart');
+stop = document.getElementById('btnStop');
+
+var keep = null;
+var ticker = 0;
+
+start.innerHTML = "Start";
+stop.innerHTML = "Stop";
+
+start.onclick = () => {keep = setInterval(() => {ticker = ticker+1; console.log(ticker);}, 100)}; 
+stop.onclick = () => {clearInterval(keep);};
+
