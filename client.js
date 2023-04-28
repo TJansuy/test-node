b18d5df2704a36640fe83929a025c6ea34516733
Trixi Jansuy <7642477+TJansuy@users.noreply.github.com>
2023-04-27 23:45:24 -0700

    Adding comments and fixing a bug with extra intervals being created

--- client.js
+++ client.js
@@ -1,17 +1,23 @@
 console.log("success");
 
+// Testing getElementById
 e = document.getElementById('foo');
 e.innerHTML = "Updated by client.js";
 
+// Find the buttons
 start = document.getElementById('btnStart');
 stop = document.getElementById('btnStop');
 
+
+// Begin button Functionality
 var keep = null;
 var ticker = 0;
 
+//// Give the buttons text programatically
 start.innerHTML = "Start";
 stop.innerHTML = "Stop";
 
-start.onclick = () => {keep = setInterval(() => {ticker = ticker+1; console.log(ticker);}, 100)}; 
-stop.onclick = () => {clearInterval(keep);};
+//// Assign the onclick functionality after the script has had a chance to load.
+start.onclick = () => {if(keep === null) { keep = setInterval(() => {ticker = ticker+1; console.log(ticker); e.innerHTML = ticker;}, 100)};}; 
+stop.onclick = () => {clearInterval(keep); keep = null;};
 
